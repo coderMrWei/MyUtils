451ea5fc82ced2a1a15a5b8809f8d7c8be2a9e8e
yibao.wei <yibao.wei@qunar.com>
2016-03-01 16:42:28 +0800

    小工具类

---
+++ src/test/java/MyUtilsTestDrive.java
@@ -0,0 +1,10 @@
+/**
+ * Created by yibaowei on 16-3-1.
+ */
+public class MyUtilsTestDrive {
+    public static void main(String [] args){
+        int[] arr = {1,2,3,4,5};
+        int[] target = {3,4,5};
+        System.out.println(MyUtils.indexOf(arr, target));
+    }
+}
